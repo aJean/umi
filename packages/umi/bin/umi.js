c9bc2490502f025642e0b3f7c53ce00cda619190
qiaoyue <qiaoyue@baidu.com>
2021-01-03 14:33:28 +0800

    update

--- packages/umi/bin/umi.js
+++ packages/umi/bin/umi.js
@@ -4,6 +4,7 @@ const resolveCwd = require('resolve-cwd');
 
 const { name, bin } = require('../package.json');
 const localCLI = resolveCwd.silent(`${name}/${bin['umi']}`);
+// 本地还是全局
 if (!process.env.USE_GLOBAL_UMI && localCLI && localCLI !== __filename) {
   const debug = require('@umijs/utils').createDebug('umi:cli');
   debug('Using local install of umi');
